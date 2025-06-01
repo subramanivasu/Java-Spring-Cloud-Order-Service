06ac1e3d3f0aa1d30982718009eb72b0e801f9fa
subramanivasu <subramaniyanvasu@gmail.com>
2025-06-01 21:27:10 +0530

    Added circuit breaker pattern to inventory client

--- src/main/java/com/project/ecommerce/microservices/order/config/RestClientConfig.java
+++ src/main/java/com/project/ecommerce/microservices/order/config/RestClientConfig.java
@@ -1,12 +1,20 @@
 package com.project.ecommerce.microservices.order.config;
 
 import com.project.ecommerce.microservices.order.client.InventoryClient;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.util.Timeout;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
@@ -18,10 +26,25 @@ public class RestClientConfig {
     public InventoryClient inventoryClient(){
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
+                .requestFactory(getClientRequestFactory())
                 .build();
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
         return httpServiceProxyFactory.createClient(InventoryClient.class);
 
+    }
+
+    private ClientHttpRequestFactory getClientRequestFactory() {
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setDefaultRequestConfig(
+                       RequestConfig.custom()
+                                .setConnectTimeout(Timeout.ofSeconds(3))
+                                .setResponseTimeout(Timeout.ofSeconds(3))
+                                .build()
+                )
+                .build();
+
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        return factory;
     }
 }
